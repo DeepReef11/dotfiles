85d0071475267d52c7813d140514611f4a808946
ok <ok@ok>
2025-02-03 09:40:49 -0500

    mute with 0

---
+++ .config/hypr/scripts/microphone-mute.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+current_volume=$(wpctl get-volume @DEFAULT_AUDIO_SOURCE@ | awk '{print $2}' | awk '{printf "%.2f", $1}')
+
+if [ "$current_volume" = "0.00" ]; then
+    wpctl set-volume @DEFAULT_AUDIO_SOURCE@ 100
+    notify-send "Microphone" "Unmuted (100%)" -i audio-input-microphone
+else
+    wpctl set-volume @DEFAULT_AUDIO_SOURCE@ 0
+    notify-send "Microphone" "Muted (0%)" -i audio-input-microphone-muted
+fi
