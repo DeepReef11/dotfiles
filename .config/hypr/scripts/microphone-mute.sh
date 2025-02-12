f4bb004c16f024a0c96e2faa554680a659953fa6
ok <ok@ok>
2025-02-12 09:22:24 -0500

    Fixed signal-desktop and brave, muting mic sound, lauch nc,

--- .config/hypr/scripts/microphone-mute.sh
+++ .config/hypr/scripts/microphone-mute.sh
@@ -3,7 +3,7 @@
 current_volume=$(wpctl get-volume @DEFAULT_AUDIO_SOURCE@ | awk '{print $2}' | awk '{printf "%.2f", $1}')
 
 if [ "$current_volume" = "0.00" ]; then
-    wpctl set-volume @DEFAULT_AUDIO_SOURCE@ 100
+    wpctl set-volume @DEFAULT_AUDIO_SOURCE@ 1
     notify-send "Microphone" "Unmuted (100%)" -i audio-input-microphone
 else
     wpctl set-volume @DEFAULT_AUDIO_SOURCE@ 0
