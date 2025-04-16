2ca8845b5b6a4b1ea7bd3435d34413c9ccee4d39
ok <ok@ok>
2025-04-16 15:09:57 -0400

    css update

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -25,12 +25,14 @@
   #     # Any additional configuration
   #   };
 
-  programs.neovim.enable = true;
+  programs.neovim.enable = false;
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = [
-
+    # TODO: nvf
     inputs.nvf-config.packages.${pkgs.system}.nvf-config
+
+
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     # pkgs.hello
@@ -51,7 +53,10 @@
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
+
   home.file = {
+
+    ".config/nvim".source = ~/.config/nvim;
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
--- .config/home-manager/nvf-config/config/mini.nix
+++ .config/home-manager/nvf-config/config/mini.nix
@@ -5,74 +5,111 @@
   ...
 }:
 {
-  vim = {
-    mini = {
-      icons.enable = true;
-      statusline.enable = true;
-      tabline.enable = true;
-      git.enable = true;
-      diff.enable = true;
-      align.enable = true;
-      notify.enable = true;
-      files.enable = true;
-      indentscope = {
-        enable = true;
-        setupOpts.draw.animation = lib.generators.mkLuaInline ''require("mini.indentscope").gen_animation.none()'';
-      };
-      operators.enable = true;
-      starter.enable = true;
-      trailspace.enable = true;
-      jump2d = {
-        enable = true;
-        setupOpts = {
-          mappings.start_jumping = "<cr>";
-          view.dim = true;
-          silent = true;
-        };
-      };
-      hipatterns = {
-        enable = true;
-        setupOpts.highlighters =
-          let
-            mkPattern = pattern: "%f[%w]()${pattern}()%f[%W]";
-            mkHi = pattern: group: {
-              inherit group;
-              pattern = mkPattern pattern;
-            };
-          in
-          {
-            todo = mkHi "TODO" "MiniHipatternsTodo";
-            hack = mkHi "HACK" "MiniHipatternsHack";
-            note = mkHi "NOTE" "MiniHipatternsNote";
-            fixme = mkHi "FIXME" "MiniHipatternsFixme";
-            hex_color = lib.generators.mkLuaInline ''require("mini.hipatterns").gen_highlighter.hex_color()'';
-            base16_color =
-              lib.generators.mkLuaInline # lua
-                ''
-                  {
-                    pattern = "base0[%dA-F]",
-                    group = function(_, match)
-                      local words = ${lib.nvim.lua.toLuaObject colorScheme'}
-                      local hex = words[match]
-                      if hex == nil then return nil end
-                      return MiniHipatterns.compute_hex_color_group(hex, bg)
-                    end,
-                  }
-                '';
-          };
-      };
-      comment.enable = true;
-      move.enable = true;
-      pairs.enable = true;
-      splitjoin.enable = true;
-      bracketed.enable = true;
-    };
-
-    keymaps = [
-      (util.mkKeymap' "n" "<tab>" ":bn<cr>")
-      (util.mkKeymap' "n" "<S-tab>" ":bp<cr>")
-      (util.mkKeymap' "n" "<C-c>" ":bd<cr>")
-      (util.mkKeymap "n" "<leader>fm" ":lua MiniFiles.open()<cr>" "Open mini.files")
-    ];
-  };
+#   vim = {
+#     mini = {
+#       # Individual mini plugins
+#       align.enable = true;
+#       ai.enable = true;
+#       files = {
+#         enable = true;
+#         # Custom keymaps for files will be handled separately
+#       };
+#       move.enable = true;
+#       splitjoin.enable = true;
+#       surround = {
+#         enable = true;
+#         setupOpts.mappings = {
+#           add = "gsa";
+#           delete = "gsd";
+#           find = "gsf";
+#           find_left = "gsF";
+#           highlight = "gsh";
+#           replace = "gsr";
+#           update_n_lines = "gsn";
+#         };
+#       };
+#       misc = {
+#         enable = true;
+#         setupOpts = lib.generators.mkLuaInline ''require("mini.misc").setup_restore_cursor()'';
+#       };
+#       indentscope = {
+#         enable = true;
+#         setupOpts = {
+#           draw = {
+#             delay = 0;
+#             animation = lib.generators.mkLuaInline ''require("mini.indentscope").gen_animation.none()'';
+#             priority = 2;
+#           };
+#           mappings = {
+#             object_scope = "ii";
+#             object_scope_with_border = "ai";
+#             goto_top = "[i";
+#             goto_bottom = "]i";
+#           };
+#           options = {
+#             border = "both";
+#             indent_at_cursor = false;
+#             try_as_border = false;
+#           };
+#           symbol = "╎";
+#         };
+#       };
+#       animate = {
+#         enable = true;
+#         setupOpts = {
+#           scroll.enable = false;
+#           cursor = {
+#             enable = true;
+#             timing = lib.generators.mkLuaInline ''
+#               require('mini.animate').gen_timing.cubic({
+#                 easing = "out",
+#                 duration = 400,
+#                 unit = "total"
+#               })
+#             '';
+#           };
+#         };
+#       };
+#     };
+#
+#     keymaps = [
+#       (util.mkKeymap "n" "<leader>e" "<cmd>lua MiniFiles.open(vim.api.nvim_buf_get_name(0))<cr>" "Open minifiles at buffer location")
+#       (util.mkKeymap "n" "<leader>E" "<cmd>lua MiniFiles.open()<cr>" "Open minifiles at cwd")
+#       (util.mkKeymap "n" "<M-Up>" "<cmd>lua MiniMove.move_line('up')<cr>" "MiniMove line")
+#       (util.mkKeymap "n" "<M-Down>" "<cmd>lua MiniMove.move_line('down')<cr>" "MiniMove line")
+#       (util.mkKeymap "n" "<M-Left>" "<cmd>lua MiniMove.move_line('left')<cr>" "MiniMove line")
+#       (util.mkKeymap "n" "<M-Right>" "<cmd>lua MiniMove.move_line('right')<cr>" "MiniMove line")
+#       (util.mkKeymap "x" "<M-Up>" "<cmd>lua MiniMove.move_selection('up')<cr>" "MiniMove selection")
+#       (util.mkKeymap "x" "<M-Down>" "<cmd>lua MiniMove.move_selection('down')<cr>" "MiniMove selection")
+#       (util.mkKeymap "x" "<M-Left>" "<cmd>lua MiniMove.move_selection('left')<cr>" "MiniMove selection")
+#       (util.mkKeymap "x" "<M-Right>" "<cmd>lua MiniMove.move_selection('right')<cr>" "MiniMove selection")
+#     ];
+#
+#     autoGroups = {
+#       mini_files_mappings = {
+#         clear = true;
+#         events = ["User"];
+#         patterns = ["MiniFilesBufferCreate"];
+#         callback = lib.generators.mkLuaInline ''
+#           function(args)
+#             local map_buf = function(lhs, rhs)
+#               vim.keymap.set("n", lhs, rhs, { buffer = args.data.buf_id })
+#             end
+#
+#             local go_in_plus = function()
+#               for _ = 1, vim.v.count1 do
+#                 MiniFiles.go_in({ close_on_file = true })
+#               end
+#             end
+#
+#             map_buf("<CR>", go_in_plus)
+#             map_buf("<Right>", go_in_plus)
+#             map_buf("<BS>", MiniFiles.go_out)
+#             map_buf("<Left>", MiniFiles.go_out)
+#             map_buf("<Esc>", MiniFiles.close)
+#           end
+#         '';
+#       };
+#     };
+#   };
 }
